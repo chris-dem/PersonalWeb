25098bd027db02e4ba2004ec30ce8012f15d888b
StepToDev <csdeme102@gmail.com>
2022-04-22 15:11:10 +0100

    Fixed dir

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Testing } from './components/Fetch';
+
+const App : React.FC = () => {
+  return (
+    <div className="App">
+      <Testing />
+    </div>
+  );
+}
+
+export default App;
